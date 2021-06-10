1e64f5c6e5a411ad4fe697d2c612c8583b508416
Secondnd <2econdnd@gmail.com>
2021-06-10 17:05:04 +0200

    Main Menu On Start
    Basic UI main menu

---
+++ RocketBoost/Assets/Scripts/Menu/PlayGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex+1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);   
+    }
+    public void EndGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit(1);
+    }
+}
